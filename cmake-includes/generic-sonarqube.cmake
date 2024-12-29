f1a3339f68f2e4d5272830218541445526b9c035
Simon Gleissner <simon@gleissner.de>
2024-12-29 16:26:08 +0100

    bugfix gcov/gcovr/sonarqube code coverage
    debug C++20 module code coverage

--- cmake-includes/generic-sonarqube.cmake
+++ cmake-includes/generic-sonarqube.cmake
@@ -4,7 +4,7 @@
 option(SONARQUBE "generate configuration for SonarQube" OFF)
 if (SONARQUBE)
     # add code coverage
-    add_compile_options(-g -O0 --coverage -fprofile-abs-path)
+    add_compile_options(-g -O1 --coverage -fprofile-abs-path)
     add_link_options(--coverage)
 
     file(REMOVE ${CMAKE_SOURCE_DIR}/sonar-project.properties)
